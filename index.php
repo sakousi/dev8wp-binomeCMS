ab7033a9da916c777bf5d60438226a93c2cbad1e
theau <theaudugueperoux@gmail.com>
2023-01-30 09:19:11 +0100

    index dump

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <?php get_header(); ?>
 	
 	<div>
-		<?php if ( have_posts() ) : ?>
-			
-			<?php
-			// Début the loop.
-			while ( have_posts() ) :
-				the_post();
-				?>
-				<!-- CE QUE VOUS VOULEZ AFFICHER -->
-			<?php endwhile; ?>
-		
-		<?php else : ?>
-		
-		<?php endif; ?>
 	
 	</div>
 
