8fa846e968f46c72809f7e2eb368ace0e8cb62e3
theau <theaudugueperoux@gmail.com>
2023-01-30 14:11:23 +0100

    update form main

--- functions.php
+++ functions.php
@@ -17,3 +17,18 @@
 	
 	
 	add_filter('body_class', 'my_plugin_body_class');
+
+
+// Ajouter un emplacement de menu
+	function theme_register_nav_menu(){
+		register_nav_menus( array(
+			'menu_principal' => __( 'Menu Principal', 'text_domain' ),
+        ));
+	}
+	add_action( 'after_setup_theme', 'theme_register_nav_menu', 0 );
+	
+// Ajouter page d'options ACF
+
+if (function_exists('acf_add_options_page')) {
+	acf_add_options_page();
+}
--- header.php
+++ header.php
@@ -8,7 +8,27 @@
 </head>
 
 
-<body <?php body_class(); ?>>
+<body <?php body_class();?>>
+<main class="bg-gray-400 mx-64">
+<nav class="flex justify-center">
 
+    <ul class="bg-stone-50 max-w-min max-w-[50%] flex space-x-6">
+    <?php
+	    // On récupère la liste des menus
+	    $menuLocations = get_nav_menu_locations();
+	
+	    // On récupère l'ID de notre menu principal
+	    $menuID = $menuLocations['menu_principal'];
+     
+	    // On récupère les liens de ce menu
+	    $menu = wp_get_nav_menu_items($menuID);
+     
+	    // On boucle dans les liens et on les affiche
+	    foreach ( $menu as $navItem ) {
+            echo '<li><a href="'.$navItem->url.'" title="'.$navItem->title.'">'.$navItem->title.'</a></li>';
+        }
+    ?>
+    </ul>
+</nav>
 
 <?php wp_body_open(); ?>
