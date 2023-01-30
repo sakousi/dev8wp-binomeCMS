a3dc818461c8476f9fd8cda65c7b9277a178e1bb
theau <theaudugueperoux@gmail.com>
2023-01-30 16:45:15 +0100

    :bug: resolve fron-page ACF

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <?php wp_footer(); ?>
 </main>
-<footer class="bg-sky-600 grid grid-cols-3 mx-28 py-12">
+<footer class="bg-cyan-900 grid grid-cols-3 mx-28 py-12">
     <div class="flex justify-center items-center">
         <img class="object-cover h-28" src="<?= get_field('logo', 'options') ?>" alt="Logo">
     </div>
@@ -9,7 +9,7 @@
         <?php if(have_rows('campus', 'options')): ?>
             <ul class="grid grid-cols-2">
                 <?php while (have_rows('campus', 'options')): the_row() ?>
-                <li class="py-2"><a class="text-white" href=""><?php the_sub_field('city'); ?></a></li>
+                <li class="py-2"><a class="text-white text-lg" href=""><?php the_sub_field('city'); ?></a></li>
                 <?php endwhile; ?>
             </ul>
         <?php endif; ?>
