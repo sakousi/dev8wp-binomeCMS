e31e0103df8587523fdfef61277d257b4dcc0c69
theau <theaudugueperoux@gmail.com>
2023-01-30 14:16:55 +0100

    :alembic: Final about + Nav style modif

--- about.php
+++ about.php
@@ -1,10 +1,10 @@
 <?php /* Template Name: About */ ?>
 <?php get_header(); ?>
-<header>
+<header class="p-4">
 	<h1 class="text-4xl"><?php the_field('title'); ?></h1>
 </header>
 <section>
-    <div>
+    <div class="p-4">
         <?= get_field('desc') ?>
     </div>
 	<div class="grid grid-cols-2 grid-rows-2 gap-4">
@@ -18,5 +18,8 @@
 		<img class="" src="<?= $images[1] ?>" alt="">
 		<img class="" src="<?= $images[2] ?>" alt="">
 	</div>
+    <div class="p-4">
+        <?= get_field('desc_2') ?>
+    </div>
 </section>
 <?php get_footer(); ?>
